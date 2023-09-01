664fb4efb34c08249e4c1430c4c40bc562f2fc8b
franco-dev <francoacm1011000@gmail.com>
2023-08-31 23:51:24 -0500

    update figures

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+hideMenuOnClickLinks()
+
 function hideMenuOnClickLinks() {
     const [...anchorsMenu] = document.querySelectorAll("header aside a")
     const inputCheckMenu = document.querySelector("header input#burger")
 
+    console.log(inputCheckMenu)
+
     anchorsMenu.forEach(anchor => {
         anchor.addEventListener("click", () => {
+            console.log("clicked")
             inputCheckMenu.checked = false
         })
     })
